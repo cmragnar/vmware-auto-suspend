b62e381f54cb3061554f6e3aabf22dcb5c9e5fd0
Christian <52965048+cmragnar@users.noreply.github.com>
2025-06-23 07:14:48 +0200

    VMwareSuspend.bat

--- SuspendRunningVMs.bat
+++ SuspendRunningVMs.bat
@@ -1,47 +1,35 @@
 @echo off
-echo SuspendRunningVMs Command (x64)...
-REM https://stackoverflow.com/questions/20691060/echo-a-blank-empty-line-to-the-console-from-a-windows-batch-file
-echo(
-echo Ideas for improving this? Visit https://github.com/fatso83/vmware-auto-suspend
-echo(
-echo(
+REM VMware Auto-Suspend - Handles multiple VMs properly
 
-SETLOCAL
-REM Specify where vmrun.exe can be located
-SET WSPath="C:\Program Files (x86)\VMware\VMware Workstation"
+SET VMWARE_PATH="C:\Program Files (x86)\VMware\VMware Player"
+IF NOT EXIST %VMWARE_PATH%\vmrun.exe (
+    SET VMWARE_PATH="C:\Program Files (x86)\VMware\VMware Workstation"
+)
+cd /d %VMWARE_PATH%
+IF NOT EXIST vmrun.exe exit /b 1
 
-REM Get the list of currently running VMs
-%WSPath%\vmrun.exe list > %temp%\vmlist.txt
+REM Count running VMs
+for /f "tokens=4" %%G in ('vmrun.exe list ^| find "Total running"') DO SET VM_COUNT=%%G
+echo Found %VM_COUNT% running VMs
 
-REM Suspend all running VMs
-FOR /F "delims=* skip=1" %%v IN (%temp%\vmlist.txt) DO CALL :SuspendVM "%%v"
+REM Suspend all VMs in parallel (faster for multiple VMs)
+for /f "skip=1 delims=" %%G in ('vmrun.exe list') DO (
+    echo Suspending: %%G
+    START "" /B vmrun.exe suspend "%%G"
+)
 
-:WaitLoop
-echo Waiting for the VMs to suspend...
-REM Pause until no more VMs are running
-%WSPath%\vmrun.exe list | FIND "Total running VMs: 0"
-IF NOT ERRORLEVEL 1 GOTO End
-timeout /t 5/nobreak
-GOTO WaitLoop
- 
-:End
-echo End of script; all VMs suspended.
-ENDLOCAL
-GOTO :EOF
- 
-REM Suspend a VM
-:SuspendVM
-echo Suspending VM %1
-%WSPath%\vmrun.exe suspend %1
-REM Allow some time after suspend call (allow disk to write vmem).
-echo Wait a little bit for the VM to commit...
-timeout /t 10 /nobreak
-GOTO :EOF
- 
-REM Resume a VM (not used now, but may have use in future)
-:ResumeVM
-echo Starting VM %1
-%WSPath%\vmrun.exe start %1
-GOTO :EOF
- 
-:EOF
+REM Wait for all VMs to suspend (10 seconds per VM max)
+SET /A WAIT_TIME=%VM_COUNT%*10
+IF %WAIT_TIME% GTR 60 SET WAIT_TIME=60
+echo Waiting up to %WAIT_TIME% seconds for VMs to suspend...
+timeout /t %WAIT_TIME% /nobreak >nul
+
+REM Verify all VMs are suspended
+vmrun.exe list | find "Total running VMs: 0"
+IF ERRORLEVEL 1 (
+    echo Warning: Some VMs may still be running!
+    REM Force suspend any remaining VMs
+    for /f "skip=1 delims=" %%G in ('vmrun.exe list') DO vmrun.exe suspend "%%G" hard
+)
+
+exit /b 0
